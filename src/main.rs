6592d61696574228d9700a1fd917bcac8f4a5e2b
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-06-18 13:52:44 +0200

    minimize

--- src/main.rs
+++ src/main.rs
@@ -1,175 +1,59 @@
-mod audio;
-mod states;
-
 use amethyst::{
-    assets::Processor,
-    audio::{AudioBundle, DjSystem},
-    core::{frame_limiter::FrameRateLimitStrategy, transform::TransformBundle},
-    ecs::{Component, DenseVecStorage, ReadExpect, Resources, SystemData},
-    input::{InputBundle, StringBindings},
+    core::transform::TransformBundle,
+    ecs::Resources,
     prelude::*,
     renderer::{
-        pass::DrawFlat2DDesc, types::DefaultBackend, Factory, Format, GraphBuilder, GraphCreator,
-        Kind, RenderGroupDesc, RenderingSystem, SpriteSheet, SubpassBuilder,
+        types::DefaultBackend, Factory, GraphBuilder, GraphCreator,
+        RenderingSystem
     },
-    ui::{DrawUiDesc, UiBundle},
-    utils::application_root_dir,
-    window::{ScreenDimensions, Window, WindowBundle},
+    window::{WindowBundle, DisplayConfig},
 };
 
-use crate::{audio::Music, states::Start};
+pub struct Start;
 
-use std::time::Duration;
+impl SimpleState for Start {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
 
-const ARENA_HEIGHT: f32 = 100.0;
-const ARENA_WIDTH: f32 = 100.0;
-const PADDLE_HEIGHT: f32 = 16.0;
-const PADDLE_WIDTH: f32 = 4.0;
-const PADDLE_VELOCITY: f32 = 75.0;
-
-const BALL_VELOCITY_X: f32 = 75.0;
-const BALL_VELOCITY_Y: f32 = 50.0;
-const BALL_RADIUS: f32 = 2.0;
-
-const AUDIO_MUSIC: &'static [&'static str] = &[
-    "audio/Computer_Music_All-Stars_-_Wheres_My_Jetpack.ogg",
-    "audio/Computer_Music_All-Stars_-_Albatross_v2.ogg",
-];
-const AUDIO_BOUNCE: &'static str = "audio/bounce.ogg";
-const AUDIO_SCORE: &'static str = "audio/score.ogg";
+    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        panic!();
+    }
+}
 
 fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-    let app_root = application_root_dir()?;
-
-    let display_config_path = app_root.join("resources/display.ron");
-
-    let key_bindings_path = {
-        if cfg!(feature = "sdl_controller") {
-            app_root.join("resources/input_controller.ron")
-        } else {
-            app_root.join("resources/input.ron")
-        }
-    };
-
-    let assets_dir = app_root.join("examples/assets/");
-
     let game_data = GameDataBuilder::default()
         // The WindowBundle provides all the scaffolding for opening a window
-        .with_bundle(WindowBundle::from_config_path(display_config_path))?
+        .with_bundle(WindowBundle::from_config(DisplayConfig::default()))?
         // Add the transform bundle which handles tracking entity positions
         .with_bundle(TransformBundle::new())?
-        // A Processor system is added to handle loading spritesheets.
-        .with(
-            Processor::<SpriteSheet>::new(),
-            "sprite_sheet_processor",
-            &[],
-        )
-        .with_bundle(AudioBundle::default())?
-        .with(
-            DjSystem::new(|music: &mut Music| music.music.next()),
-            "dj_system",
-            &[],
-        )
         // The renderer must be executed on the same thread consecutively, so we initialize it as thread_local
         // which will always execute on the main thread.
         .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
-            ExampleGraph::default(),
+            ExampleGraph,
         ));
 
-    let mut game = Application::build(assets_dir, Start)?
+    let mut game = Application::build(".Application/", Start)?
         .build(game_data)?;
 
     game.run();
     Ok(())
 }
 
-pub struct Ball {
-    pub velocity: [f32; 2],
-    pub radius: f32,
-}
-
-impl Component for Ball {
-    type Storage = DenseVecStorage<Self>;
-}
-
-#[derive(PartialEq, Eq)]
-pub enum Side {
-    Left,
-    Right,
-}
-
-pub struct Paddle {
-    pub velocity: f32,
-    pub side: Side,
-    pub width: f32,
-    pub height: f32,
-}
-
-impl Paddle {
-    pub fn new(side: Side) -> Paddle {
-        Paddle {
-            velocity: 1.0,
-            side: side,
-            width: 1.0,
-            height: 1.0,
-        }
-    }
-}
-
-impl Component for Paddle {
-    type Storage = DenseVecStorage<Self>;
-}
-
-#[derive(Default)]
-pub struct ScoreBoard {
-    score_left: i32,
-    score_right: i32,
-}
-
-impl ScoreBoard {
-    pub fn new() -> ScoreBoard {
-        ScoreBoard {
-            score_left: 0,
-            score_right: 0,
-        }
-    }
-}
-
-// This graph structure is used for creating a proper `RenderGraph` for rendering.
-// A renderGraph can be thought of as the stages during a render pass. In our case,
-// we are only executing one subpass (DrawFlat2D, or the sprite pass). This graph
-// also needs to be rebuilt whenever the window is resized, so the boilerplate code
-// for that operation is also here.
-#[derive(Default)]
-struct ExampleGraph {
-    dimensions: Option<ScreenDimensions>,
-    surface_format: Option<Format>,
-    dirty: bool,
-}
+struct ExampleGraph;
 
 impl GraphCreator<DefaultBackend> for ExampleGraph {
     // This trait method reports to the renderer if the graph must be rebuilt, usually because
     // the window has been resized. This implementation checks the screen size and returns true
     // if it has changed.
-    fn rebuild(&mut self, res: &Resources) -> bool {
-        // Rebuild when dimensions change, but wait until at least two frames have the same.
-        let new_dimensions = res.try_fetch::<ScreenDimensions>();
-        use std::ops::Deref;
-        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
-            self.dirty = true;
-            self.dimensions = new_dimensions.map(|d| d.clone());
-            return false;
-        }
-        return self.dirty;
+    fn rebuild(&mut self, _res: &Resources) -> bool {
+        false
     }
 
     // This is the core of a RenderGraph, which is building the actual graph with subpasses and target
     // images.
     fn builder(
         &mut self,
-        factory: &mut Factory<DefaultBackend>,
-        res: &Resources,
+        _factory: &mut Factory<DefaultBackend>,
+        _res: &Resources,
     ) -> GraphBuilder<DefaultBackend, Resources> {
         GraphBuilder::new()
     }
